044bd383254f7c756cf47c80e580c4ee5ecf5e20
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-04-08 14:50:51 +0200

    Update GameOver.js

--- reactgame/src/GameComponents/GameOver.js
+++ reactgame/src/GameComponents/GameOver.js
@@ -1,8 +1,10 @@
 function GameOver(props) {
   return (
     <>
-      <h1>Game Over</h1>
+      <h1>Game Over 😢</h1>
       <h2>The answer was: {props.answer}</h2>
+      <h4>Below is what you've guessed.</h4>
+      <h5>{props.guesses}</h5>
       <button className="submitBtn" onClick={props.function}>
         Play again?
       </button>
