d125d80ad77116785d15a80040a1966f2b66e141
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-04-09 16:46:17 +0200

    prepare for tests

---
+++ test/test.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from "@jest/globals";
+import app from "../src/app.js";
+
+// Prepare some tests here... To be continued...
+
+// describe("Describe test here", () => {
+//   test("What should be tested", () => {
+//     const output = algorithmA("HALLÅ", "CYKLA");
+
+//     expect(output[0].result).toEqual("incorrect");
+//     expect(output[1].result).toEqual("misplaced");
+//     expect(output[2].result).toEqual("incorrect");
+//     expect(output[3].result).toEqual("correct");
+//     expect(output[4].result).toEqual("incorrect");
+//   });
+// });
