641fcef6b0d597016a80b5f79fc25e7eb4b2941b
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-04-09 12:16:02 +0200

    added a navbar

--- reactgame/src/index.js
+++ reactgame/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
+import InitHeader from "./InitComponents/InitHeader";
 import Init from "./Init";
 
 // Render the Init component. Letting the user choose word settings before the game is started.
 ReactDOM.render(
   <div className="App">
+    <InitHeader />
     <Init />
   </div>,
   document.getElementById("root")
