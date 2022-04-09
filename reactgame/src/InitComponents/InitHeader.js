84d3c094afa9040f1c0c369af6c582ee2bec2f1e
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-04-09 12:16:25 +0200

    Create InitHeader.js

---
+++ reactgame/src/InitComponents/InitHeader.js
@@ -0,0 +1,12 @@
+function InitHeader() {
+
+  // Render a menu navbar.
+  return (
+    <div className="HeaderNavbar">
+      <a href="/">-Play-</a> | <a href="/highscore">Highscore</a> |
+      <a href="/about"> About</a>
+    </div>
+  );
+}
+
+export default InitHeader;
